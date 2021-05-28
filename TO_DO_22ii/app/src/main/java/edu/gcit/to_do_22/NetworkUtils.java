36e69e59de89c3771cac5df834726f2c58c4ed4a
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2021-05-28 21:58:20 +0600

    Todo

---
+++ TO_DO_22ii/app/src/main/java/edu/gcit/to_do_22/NetworkUtils.java
@@ -0,0 +1,93 @@
+package edu.gcit.to_do_22;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+public class NetworkUtils {
+    private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
+
+    //base url for books api
+    private static final String BOOK_BASE_URL = "https://www.googleapis.com/books/v1/volumes?";
+    //parameter for the search string
+    private static final String QUERY_PARAM ="q";
+    //limits search limits
+    private static final String MAX_RESULTS = "maxResults";
+    private static final String PRINT_TYPE = "printType";
+
+    public static String getBookInfo(String queryString)
+    {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String bookJSONString = null;
+
+        try
+        {
+            Uri builtURI = Uri.parse(BOOK_BASE_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARAM, queryString)
+                    .appendQueryParameter(MAX_RESULTS, "10")
+                    .appendQueryParameter(PRINT_TYPE, "books")
+                    .build();
+            URL requestURL = new URL(builtURI.toString());
+
+            urlConnection = (HttpURLConnection) requestURL.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            //Get the Inputstream
+
+            InputStream inputStream = urlConnection.getInputStream();
+
+             //create a buffer reader from that input stream
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            StringBuilder builder = new StringBuilder();
+
+            String line;
+            while ((line = reader.readLine())!=null)
+            {
+                builder.append(line);
+
+                builder.append("\n");
+            }
+            if(builder.length()==0)
+            {
+                return  null;
+            }
+            bookJSONString = builder.toString();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            if(urlConnection != null)
+            {
+                urlConnection.disconnect();
+            }
+            if (reader != null)
+            {
+                try {
+                    reader.close();
+
+                }catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        Log.d(LOG_TAG, bookJSONString);
+        return  bookJSONString;
+
+    }
+
+}
